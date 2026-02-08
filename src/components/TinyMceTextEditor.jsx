2a0240bf87fd1e2ae3d40ca753a5f29c313e03cb
zahirul <zahirulopel@gmail.com>
2026-02-08 12:00:35 +0600

    add form to add new post

--- src/components/TinyMceTextEditor.jsx
+++ src/components/TinyMceTextEditor.jsx
@@ -1,63 +1,43 @@
 import { Editor } from "@tinymce/tinymce-react";
-import { useRef } from "react";
 
-const TinyMceTextEditor = ({ onEditorContent }) => {
-  const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      let content = editorRef.current.getContent();
-      onEditorContent(content);
-      // clear the editor after submit:
-      // editorRef.current.resetContent("");
-      // editorRef.current.setDirty(false);
-    }
-  };
-
+const TinyMceTextEditor = ({ editorRef }) => {
   return (
-    <div className="flex flex-col gap-4 p-5">
-      <Editor
-        apiKey={import.meta.env.VITE_TINY_MCE_TEXT_EDITOR}
-        onInit={(evt, editor) => (editorRef.current = editor)}
-        initialValue=""
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            "advlist",
-            "autolink",
-            "lists",
-            "link",
-            "image",
-            "charmap",
-            "preview",
-            "anchor",
-            "searchreplace",
-            "visualblocks",
-            "code",
-            "fullscreen",
-            "insertdatetime",
-            "media",
-            "table",
-            "code",
-            "help",
-            "wordcount",
-          ],
-          toolbar:
-            "undo redo | blocks | " +
-            "bold italic forecolor | alignleft aligncenter " +
-            "alignright alignjustify | bullist numlist outdent indent | " +
-            "removeformat | help",
-          content_style:
-            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-        }}
-      />
-      <button
-        className="rounded border border-neutral-300 px-5 py-2 shadow-sm transition duration-300 hover:cursor-pointer hover:bg-gray-100 lg:self-start"
-        onClick={log}
-      >
-        Publish
-      </button>
-    </div>
+    <Editor
+      apiKey={import.meta.env.VITE_TINY_MCE_TEXT_EDITOR}
+      onInit={(evt, editor) => (editorRef.current = editor)}
+      initialValue="What do you want to talk about?"
+      init={{
+        height: 500,
+        menubar: false,
+        plugins: [
+          "advlist",
+          "autolink",
+          "lists",
+          "link",
+          "image",
+          "charmap",
+          "preview",
+          "anchor",
+          "searchreplace",
+          "visualblocks",
+          "code",
+          "fullscreen",
+          "insertdatetime",
+          "media",
+          "table",
+          "code",
+          "help",
+          "wordcount",
+        ],
+        toolbar:
+          "undo redo | blocks | " +
+          "bold italic forecolor | alignleft aligncenter " +
+          "alignright alignjustify | bullist numlist outdent indent | " +
+          "removeformat | help",
+        content_style:
+          "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+      }}
+    />
   );
 };
 export default TinyMceTextEditor;
--- src/pages/WriteStories/write-stories.jsx
+++ src/pages/WriteStories/write-stories.jsx
@@ -1,11 +1,60 @@
+import { useRef } from "react";
 import TinyMceTextEditor from "../../components/TinyMceTextEditor";
 const WriteStories = () => {
-  const handleEditorContent = (content) => {
-    console.log(content);
+  const editorRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (editorRef.current) {
+      let content = editorRef.current.getContent();
+      console.log(content);
+      // clear the editor after submit:
+      // editorRef.current.resetContent("");
+      // editorRef.current.setDirty(false);
+    }
   };
   return (
     <div>
-      <TinyMceTextEditor onEditorContent={handleEditorContent} />
+      <form className="flex flex-col gap-4 rounded-xl" onSubmit={handleSubmit}>
+        <legend className="text-2xl">New Post</legend>
+        <div className="flex flex-col gap-2 rounded-xl border border-gray-200 p-4 shadow-sm">
+          <label htmlFor="title">TITLE</label>
+          <input
+            className="rounded-xl border border-gray-300 bg-gray-100 p-2 outline-gray-500"
+            type="text"
+            name="title"
+            id="title"
+          />
+        </div>
+        <div className="flex flex-col gap-2 rounded-xl border border-gray-200 p-4 shadow-sm">
+          <label htmlFor="excerpt">Short description</label>
+          <input
+            className="rounded-xl border border-gray-300 bg-gray-100 p-2 outline-gray-500"
+            type="text"
+            name="excerpt"
+            id="excerpt"
+          />
+        </div>
+        <div className="flex flex-col gap-2 rounded-xl border border-gray-200 p-4 shadow-sm">
+          <label htmlFor="content">CONTENT</label>
+          <TinyMceTextEditor editorRef={editorRef} />
+        </div>
+        <div className="flex flex-col gap-2 rounded-xl border border-gray-200 p-4 shadow-sm">
+          <label htmlFor="tag">TAGS</label>
+          <input
+            className="rounded-xl border border-gray-300 bg-gray-100 p-2 outline-gray-500"
+            type="text"
+            name="tag"
+            id="tag"
+          />
+        </div>
+        <button
+          type="submit"
+          className="rounded border border-neutral-300 px-5 py-2 shadow-sm outline-gray-500 transition duration-300 hover:cursor-pointer hover:bg-gray-100 lg:self-start"
+        >
+          Publish
+        </button>
+      </form>
     </div>
   );
 };
